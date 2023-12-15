fb94a9baacb157ea58a91398e97d314614f6f265
Alex ThinkPad <alexander.camilleri1@gmail.com>
2023-12-15 10:32:44 +0000

    Added commentary to index page and improved code structure

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-const canvas = document.getElementById('canvas');
-const ctx = canvas.getContext('2d');
+const canvas = document.getElementById("canvas");
+const ctx = canvas.getContext("2d");
 
 // Create ball prop
 const ball = {
@@ -8,59 +8,61 @@ const ball = {
   size: 10,
   speed: 4,
   dx: 1,
-  dy: 0.5
+  dy: 0.5,
 };
 
 // Draw ball on canvas
 function drawBall() {
   ctx.beginPath();
   ctx.arc(ball.x, ball.y, ball.size, 0, Math.PI * 2, true);
-  ctx.fillStyle = '#0095dd';
+  ctx.fillStyle = "#0095dd";
   ctx.fill();
   ctx.closePath();
-};
+}
 
+// Function to move the ball
 function moveBall() {
   ball.x += ball.dx;
   ball.y += ball.dy;
 
   // Wall collision (right/left)
-  if(ball.x + ball.size > canvas.width || ball.x - ball.size < 0) {
+  if (ball.x + ball.size > canvas.width || ball.x - ball.size < 0) {
     ball.dx *= -1; // ball.dx = ball.dx * -1
   }
 
   // Wall collision (top/bottom)
-  if(ball.y + ball.size > canvas.height || ball.y - ball.size < 0) {
+  if (ball.y + ball.size > canvas.height || ball.y - ball.size < 0) {
     ball.dy *= -1;
   }
-};
+}
 
+// Push the ball (is called when canvas is clicked - see event listener)
+function pushBall(e) {
+  console.log("Pre-click dx and dy", ball.dx, ball.dy);
+  console.log("Adding 20 to dx and 5 to dy");
+  ball.dx += 20;
+  ball.dy += 5;
+  console.log("Post-click dx and dy", ball.dx, ball.dy);
+}
 
-// Draw everything
+// Draw the ball
 function draw() {
   // clear canvas
   ctx.clearRect(0, 0, canvas.width, canvas.height);
   drawBall();
-};
+}
 
-
-// Update canvas drawing and animation
+// Function to update canvas drawing and animation
 function update() {
   moveBall();
-
   // Draw everything
   draw();
-
+  // Standard callback function to update the animation frame (see docs)
   requestAnimationFrame(update);
-};
+}
 
+// Update the canvas drawing and animation
 update();
 
-function pushBall(e) {
-  console.log(ball.dx);
-  ball.dx += 20;
-  console.log(ball.dx);
-  ball.dy += 5;
-};
-
-canvas.addEventListener('click', pushBall);
+// Event listeners to run functions when evnt occurs on webpage
+canvas.addEventListener("click", pushBall);
